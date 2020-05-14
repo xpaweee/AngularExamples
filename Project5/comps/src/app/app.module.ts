e030ab0bc1c55c347809e6a8974eb3b94a4db967
xpaweee <krzysiek.kolsut@gmail.com>
2020-05-14 21:47:19 +0200

    started project5

---
+++ Project5/comps/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { CollectionsModule } from './collections/collections.module';
+import { ElementsModule } from './elements/elements.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ElementsModule,
+    CollectionsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
