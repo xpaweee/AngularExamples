67f18847ebb8d08e46b7b25793274acf8196ab10
xpaweee <krzysiek.kolsut@gmail.com>
2020-06-12 23:38:30 +0200

    Project 9 - Form validator

---
+++ Project9/creditcard/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { CardFormComponent } from './card-form/card-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
