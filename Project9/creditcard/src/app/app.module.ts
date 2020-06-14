c0364e840cdd2448fb028f8a417e2a81c08b0fe8
xpaweee <krzysiek.kolsut@gmail.com>
2020-06-14 22:48:25 +0200

    reactive forms

--- Project9/creditcard/src/app/app.module.ts
+++ Project9/creditcard/src/app/app.module.ts
@@ -4,15 +4,21 @@ import {ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CardFormComponent } from './card-form/card-form.component';
+import { InputComponent } from './input/input.component';
+import {NgxMaskModule, IConfig} from 'ngx-mask';
+
+export const options: Partial<IConfig> | (() => Partial<IConfig>) = {};
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardFormComponent
+    CardFormComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgxMaskModule.forRoot(options)
   ],
   providers: [],
   bootstrap: [AppComponent]
