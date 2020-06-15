7d90ae3c1721212cb709d67454d1f43d22d3223b
xpaweee <krzysiek.kolsut@gmail.com>
2020-06-15 22:40:34 +0200

    Project 11 - RxJs with Reactive Forms

---
+++ Project11/mathform/src/app/equation/equation.component.ts
@@ -0,0 +1,66 @@
+import { MathValidators } from './../math-validators';
+import { FormGroup, FormControl, AbstractControl } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { from } from 'rxjs';
+import { delay, filter, scan } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-equation',
+  templateUrl: './equation.component.html',
+  styleUrls: ['./equation.component.css']
+})
+export class EquationComponent implements OnInit {
+  secondsPerSolution = 0;
+
+  mathForm = new FormGroup({
+    a: new FormControl(this.randomNumber()),
+    b: new FormControl(this.randomNumber()),
+    answer: new FormControl('')
+  }, [
+    MathValidators.addition('answer', 'a', 'b')
+  ]);
+
+  constructor() { }
+
+  get a() {
+    return this.mathForm.value.a;
+  }
+
+  get b() {
+    return this.mathForm.value.b;
+  }
+
+  ngOnInit(): void {
+    // const startTime = new Date();
+    // let numberSolved = 0;
+
+    this.mathForm.statusChanges
+    .pipe(
+      filter(value => value === 'VALID'),
+      delay(500),
+      scan((acc, value) => {
+        return {
+          numberSolved: acc.numberSolved + 1,
+          startTime: acc.startTime
+        };
+      }, {numberSolved: 0, startTime: new Date()})
+    // tslint:disable-next-line:no-shadowed-variable
+    ).subscribe(({ numberSolved, startTime}) => {
+      // if  (value === 'INVALID') {       <== filter pipe zalatwia to
+      //   return;
+      // }
+      // numberSolved++;
+      this.secondsPerSolution = ( new Date().getTime() - startTime.getTime() ) / numberSolved / 1000
+      this.mathForm.setValue({
+        a: this.randomNumber(),
+        b: this.randomNumber(),
+        answer: ''
+      });
+    });
+  }
+
+  randomNumber() {
+    return Math.floor(Math.random() * 10);
+  }
+
+}
